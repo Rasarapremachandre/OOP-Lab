f1003fc11aa70097f4afa6e0b2b5c3cb6e922c04
Rasarapremachandre <140253497+Rasarapremachandre@users.noreply.github.com>
2023-07-30 23:28:18 +0530

    Add files via upload

---
+++ Lab1-Quiz1/Lab1-Quiz3/L1-Q3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Quiz3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter a second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine("The sum of the two numbers is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
